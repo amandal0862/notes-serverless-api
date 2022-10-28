175b9c7047c1e527d8092b2bc4eae42e293bc986
amandal0862 <amandal.eduu@gmail.com>
2022-10-28 14:24:39 +0530

    CRUD Operations completed

--- notes/handler.js
+++ notes/handler.js
@@ -1,32 +1,81 @@
 "use strict";
 const DynamoDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamoDB.DocumentClient({ region: "us-east-1" });
 
-module.exports.createNote = async (event) => {
+const send = (statusCode, data) => {
   return {
-    statusCode: 201,
-    body: JSON.stringify("A new note created"),
+    statusCode,
+    body: JSON.stringify(data),
   };
 };
 
-module.exports.updateNote = async (event) => {
+module.exports.createNote = async (event, context, cb) => {
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: "Notes",
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body,
+      },
+      ConditionExpression: "attribute_not_exists(notesId)",
+    };
+    await documentClient.put(params).promise();
+    cb(null, send(201, data));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
+};
+
+module.exports.updateNote = async (event, context, cb) => {
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Note with id " + notesId + " has been updated"),
-  };
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: "Notes",
+      Key: { notesId },
+      UpdateExpression: "set #title = :title, #body = :body",
+      ExpressionAttributeNames: {
+        "#title": "title",
+        "#body": "body",
+      },
+      ExpressionAttributeValues: {
+        ":title": data.title,
+        ":body": data.body,
+      },
+      ConditionExpression: "attribute_exists(notesId)",
+    };
+    await documentClient.update(params).promise();
+    cb(null, send(201, data));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
 
-module.exports.deleteNote = async (event) => {
+module.exports.deleteNote = async (event, context, cb) => {
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Note with id " + notesId + " has been deleted"),
-  };
+  try {
+    const params = {
+      TableName: "Notes",
+      Key: { notesId },
+      ConditionExpression: "attribute_exists(notesId)",
+    };
+    await documentClient.delete(params).promise();
+    cb(null, send(201, notesId));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
 
-module.exports.getAllNotes = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("All Notes returned !"),
-  };
+module.exports.getAllNotes = async (event, context, cb) => {
+  try {
+    const params = {
+      TableName: "Notes",
+    };
+    const notes = await documentClient.scan(params).promise();
+    cb(null, send(201, notes));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
